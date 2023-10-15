40ba5c0f0ff0d42bbe705b08dba48282025c73c9
Sansdraps <sandra.letessier@gmail.com>
2023-10-15 18:19:25 +0200

    ajout de l'url de l'API meteo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 function App() {
-  return ( 
-    <div className = "App" >
-    Hello 
-    </div>
+  return (
+    // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon
+    <div className="App"> Hello </div>
   );
 }
 
