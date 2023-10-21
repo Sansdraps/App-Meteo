b775f6a8e299da959ec3611ae67d92c5038b88ac
Sansdraps <sandra.letessier@gmail.com>
2023-10-21 13:03:20 +0200

    fonction fetchWeather

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ function App() {
     }
 
     getGeolocalisation();
+    fetchWeather(
+      'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon'
+    );
   }, []);
 
   const getGeolocalisation = () => {
@@ -30,10 +33,18 @@ function App() {
       }
     );
   };
-  return (
-    // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon
-    <div className="App"> Hello </div>
-  );
+
+  const fetchWeather = async (url) => {
+    setError(false);
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {}
+  };
+
+  return <div className="App"> Hello </div>;
 }
 
 export default App;
